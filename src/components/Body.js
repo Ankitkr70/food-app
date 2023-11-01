4a74ff81049d7b21e7850d25ccd344fba64762f3
AnkitKumar <ankitkr70@gmail.com>
2023-11-01 17:16:46 +0530

    implemented filter restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,28 @@
 import RestaurantItem from "./RestaurantItem";
 import { resData } from "../utils/data";
+import { useState } from "react";
 const Body = () => {
+  const [resList, setResList] = useState(resData);
+  const filterRestaurant = () => {
+    const filteredRes = resList.filter((res) => res.info.avgRating > 4);
+    setResList(filteredRes);
+  };
+
+  const clearFilterRestauant = () => {
+    setResList(resData);
+  };
   return (
     <div className="body">
-      <div className="search-bar">Search</div>
+      <div className="filter">
+        <button className="top-rated-btn btn" onClick={filterRestaurant}>
+          Top Rated Restaurants
+        </button>
+        <button className="all-res-btn btn" onClick={clearFilterRestauant}>
+          All Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resData.map((res) => (
+        {resList.map((res) => (
           <RestaurantItem resData={res?.info} key={res?.info?.id} />
         ))}
       </div>
